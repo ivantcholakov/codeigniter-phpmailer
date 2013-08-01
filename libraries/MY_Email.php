e6661cdc02585300c614881238869414920deb55
Ivan Tcholakov <ivantcholakov@gmail.com>
2013-08-01 03:17:39 +0300

    Adding a link to the repository.

--- libraries/MY_Email.php
+++ libraries/MY_Email.php
@@ -4,6 +4,7 @@
  * CodeIgniter compatible email-library powered by PHPMailer.
  * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2012-2013.
  * @license The MIT License (MIT), http://opensource.org/licenses/MIT
+ * @link https://github.com/ivantcholakov/codeigniter-phpmailer
  *
  * Tested on production sites with CodeIgniter 3.0-dev (July 2013) and
  * PHPMailer Version 5.2.1 (January 16, 2012).
