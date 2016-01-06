f1992f422286c207d7136a33f9fb929c085e5464
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-01-06 03:22:16 +0200

    RELEASE 1.1.25.

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 A CodeIgniter compatible email-library powered by PHPMailer
 ===========================================================
 
-Version: 1.1.23  
-Author: Ivan Tcholakov <ivantcholakov@gmail.com>, 2012-2015.  
+Version: 1.1.25  
+Author: Ivan Tcholakov <ivantcholakov@gmail.com>, 2012-2016.  
 License: The MIT License (MIT), http://opensource.org/licenses/MIT
 
 This library is intended to be compatible with CI 2.x and CI 3.x.
 
-Tested on CodeIgniter 3.0.3 (October 31, 2015) and with PHPMailer Version 5.2.14 (November 1, 2015).
+Tested on CodeIgniter 3.0.3+ (January 6, 2016) and with PHPMailer Version 5.2.14+ (January 6, 2016).
 
 Installation
 ------------
--- libraries/MY_Email.php
+++ libraries/MY_Email.php
@@ -2,15 +2,15 @@
 
 /**
  * CodeIgniter compatible email-library powered by PHPMailer.
- * Version: 1.1.24
- * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2012-2015.
+ * Version: 1.1.25
+ * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2012-2016.
  * @license The MIT License (MIT), http://opensource.org/licenses/MIT
  * @link https://github.com/ivantcholakov/codeigniter-phpmailer
  *
  * This library is intended to be compatible with CI 2.x and CI 3.x.
  *
- * Tested on production sites with CodeIgniter 3.0.3+ (November 25, 2015) and
- * PHPMailer Version 5.2.14+ (November 25, 2015).
+ * Tested on production sites with CodeIgniter 3.0.3+ (January 6, 2016) and
+ * PHPMailer Version 5.2.14+ (January 6, 2016).
  */
 
 class MY_Email extends CI_Email {
