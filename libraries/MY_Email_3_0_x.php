38af128a7b0f5af06cd1728461b36314e6da91f5
Ivan Tcholakov <ivantcholakov@gmail.com>
2017-11-05 21:54:55 +0200

    Making some comments more precise. #26 Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- libraries/MY_Email_3_0_x.php
+++ libraries/MY_Email_3_0_x.php
@@ -973,7 +973,7 @@ class MY_Email extends CI_Email {
     // PHPMailer's SMTP debug output.
     // How to handle debug output.
     // Options:
-    // `html` - Output escaped, line breaks converted to `<br>`, appropriate for browser output. This is default value for CodeIgniter
+    // `html` - Output escaped, line breaks converted to `<br>`, appropriate for browser output.
     // `echo` - Output plain-text as-is, should be avoid in web production
     // `error_log` - Output to error log as configured in php.ini
     // NULL or '' - default: 'echo' on CLI, 'html' otherwise.
@@ -981,7 +981,7 @@ class MY_Email extends CI_Email {
     // Alternatively, you can provide a callable expecting two params: a message string and the debug level:
     // <code>
     // function custom_debug($str, $level) {echo "debug level $level; message: $str";};
-    // set_debug_output(custom_debug);
+    // $this->email->set_debug_output('custom_debug');
     // </code>
     public function set_debug_output($handle) {
 
