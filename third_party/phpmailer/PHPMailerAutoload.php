4ad17d3669e124c4438b84ad026ef294430d816f
Ivan Tcholakov <ivantcholakov@gmail.com>
2019-01-09 20:05:14 +0200

    Suppressing the deprecation notice "__autoload() is deprecated, use spl_autoload_register() instead". Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- third_party/phpmailer/PHPMailerAutoload.php
+++ third_party/phpmailer/PHPMailerAutoload.php
@@ -42,8 +42,10 @@ if (version_compare(PHP_VERSION, '5.1.2', '>=')) {
      * Fall back to traditional autoload for old PHP versions
      * @param string $classname The name of the class to load
      */
-    function __autoload($classname)
-    {
-        PHPMailerAutoload($classname);
-    }
+    // Removed by Ivan Tcholakov, 09-JAN-2019.
+    //function __autoload($classname)
+    //{
+    //    PHPMailerAutoload($classname);
+    //}
+    //
 }
