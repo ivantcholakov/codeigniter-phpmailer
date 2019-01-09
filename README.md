97e6643e729550ebe01b6716acb8ec7ae69e94e8
Ivan Tcholakov <ivantcholakov@gmail.com>
2019-01-09 20:14:18 +0200

    RELEASE 1.3.1. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 A CodeIgniter compatible email-library powered by PHPMailer
 ===========================================================
 
-Version: 1.3.0  
+Version: 1.3.1  
 Author: Ivan Tcholakov <ivantcholakov@gmail.com>, 2012-2019.  
 License: The MIT License (MIT), http://opensource.org/licenses/MIT
 
--- libraries/MY_Email.php
+++ libraries/MY_Email.php
@@ -2,7 +2,7 @@
 
 /**
  * CodeIgniter compatible email-library powered by PHPMailer.
- * Version: 1.3.0
+ * Version: 1.3.1
  * @author Ivan Tcholakov <ivantcholakov@gmail.com>, 2012-2019.
  * @license The MIT License (MIT), http://opensource.org/licenses/MIT
  * @link https://github.com/ivantcholakov/codeigniter-phpmailer
